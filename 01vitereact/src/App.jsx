745a19b03dab7a6dde4253371af0cbc303f64394
IshitaAdhikari <adhikariishita50@gmail.com>
2024-10-16 20:48:38 +0530

    jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,14 +1,16 @@
 
 import Abc from "./abc"
 function App() {
-  
+  const username = "coding"
 
   return (
     //to retrun only one lement but in it we can add other elements by fragmenting it 
     //it is known as fragment
+    //where there is curly braces we treat it as a variables
+    //it is also known as evaluated expression
     <> 
     <Abc/>
-    <h1> learing react</h1>
+    <h1> learing react {username} </h1> 
     <p> testing fragmenting</p>
     </>
     
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+function MyApp(){
+  return(
+    <div>
+      <h1> Custom app</h1>
+    </div>
+  )
+}
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const anotherUser = "abc"
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click me to visit google',
+  //injecting another 
+  anotherUser
 )
+
+//we are here using React render
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
+)
+   
+
+//JSX is JS mix with HTML
