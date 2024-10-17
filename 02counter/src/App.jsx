e532367cadd968c35683c318794cdcfe2c636f1c
IshitaAdhikari <adhikariishita50@gmail.com>
2024-10-17 18:57:20 +0530

    hooks

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -17,14 +17,20 @@ function App() {
   //let counter = 15
 
   const addValue = () => {
-    //counter = counter + 1
-    setCounter(counter + 1)
-    console.log("clicked", Math.random()); 
+    //counter = counter + 1  
+    if(counter < 20){
+      setCounter(counter + 1)
+    } 
+    console.log("clicked", Math.random());
+    
   }
 
   const removeVlaue = () => {
-    setCounter(counter -1)
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
   }
+
 
   return (
     <>
