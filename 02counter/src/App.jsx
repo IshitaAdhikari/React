872b18aad501de7e6f5d0188195cc351a1f58585
IshitaAdhikari <adhikariishita50@gmail.com>
2024-10-17 18:41:09 +0530

    hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'  //hook comes from here
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//React decides that what will be updated in UI
+//the data is updated using hooks
+
+function App() {
+
+  let [counter, setCounter] = useState(15)
+  //counter is value, setCounter is function this makes the same as le counter
+  //by using this where there is counter it updates its value every where
+  //set counter takes the new value of the counter
+  //we can name setcounter anything we want
+
+  //let counter = 15
+
+  const addValue = () => {
+    //counter = counter + 1
+    setCounter(counter + 1)
+    console.log("clicked", Math.random()); 
+  }
+
+  const removeVlaue = () => {
+    setCounter(counter -1)
+  }
+
+  return (
+    <>
+    <h1> React </h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button
+    onClick={addValue}
+    >Add value</button>
+    <br />
+    <button
+    onClick={removeVlaue}
+    >Remove value</button>
+    </>
+  )
+}
+
+export default App
